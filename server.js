d78222617ab5c0e808278bb8f0c6446ce5b2ca12
Abdul Rafay Salfi <rafaysalfi428@gmail.com>
2021-09-23 20:25:49 +0500

    V2

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
